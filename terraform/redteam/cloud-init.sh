63094de9189847f93d8e8d50f31f83a1798061c9
Korben Kirscht <kirscht@gmail.com>
2021-06-09 19:20:29 -0700

    anything

--- terraform/redteam/cloud-init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-exec > /tmp/korben 2>&1
-sudo apt update
-sudo apt install -y docker.io
--- terraform/redteam/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "network_cidrs" {
-  type = map(string)
-  default = {
-    "RedTeam-Net" = "10.0.0.0/16",
-    "BlueTeam-Net" = "10.1.0.0/16",
-  }
-}
