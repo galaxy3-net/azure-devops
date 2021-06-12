7ecc6f5c57fa14c5813922566e16e48fd9da2bab
Korben Kirscht <kirscht@gmail.com>
2021-06-11 23:11:31 -0700

    Fixing new Terraform bug

--- terraform/blueteam/dev/backend.tf
+++ terraform/blueteam/dev/backend.tf
@@ -1,6 +1,6 @@
 
 terraform {
   backend "local" {
-    path = "/Users/korbenkirscht/Downloads/terraform/blueteam/dev/terraform.tfstate"
+    path = "/Downloads/terraform/blueteam/prod/terraform.tfstate"
   }
 }
