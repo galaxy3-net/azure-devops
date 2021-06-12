f27c52d657d151bbe91dbb2d406f0562f21a60a3
Korben Kirscht <kirscht@gmail.com>
2021-06-12 00:47:16 -0700

    Fixing new Terraform bug

--- terraform/blueteam/dev/network.tf
+++ terraform/blueteam/dev/network.tf
@@ -27,6 +27,18 @@ resource "azurerm_network_security_group" "blueteam" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    access = "Allow"
+    direction = "Inbound"
+    name = "Allow-SSH-for-Jumpbox"
+    priority = 501
+    protocol = "TCP"
+    source_port_range = "*"
+    source_address_prefix = var.home_ip
+    destination_port_range = "80"
+    destination_address_prefix = "*"
+  }
+
   tags = var.resource_tags
 }
 
