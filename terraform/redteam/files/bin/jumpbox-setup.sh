4d5024c1dcaca5a93e279c6101175a12a90d0d15
Korben Kirscht <kirscht@gmail.com>
2021-06-11 22:36:35 -0700

    Fixing new Terraform bug

--- terraform/redteam/files/bin/jumpbox-setup.sh
+++ terraform/redteam/files/bin/jumpbox-setup.sh
@@ -2,11 +2,11 @@
 
 exec | tee ~/jumpbox-setup.log
 exec 2>&1
-  # chmod 0400 /home/azureuser/.ssh/id_rsa*
+  chmod 0400 /home/azureuser/.ssh/id_rsa*
   sleep 120
   sudo apt update && sleep 120 && sudo apt install -y docker.io && \
   sudo systemctl enable docker && sudo systemctl start docker
 
-  #sudo docker pull cyberxsecurity/ansible && \
-  #sudo docker run --name ansible --mount type=bind,src=/home/vagrant/.ssh,dst=/root/.ssh --mount type=bind,src=/home/vagrant/ansible,dst=/etc/ansible  -d -t cyberxsecurity/ansible sleep infinity
+  sudo docker pull cyberxsecurity/ansible && \
+  sudo docker run --name ansible --mount type=bind,src=${HOME}/.ssh,dst=/root/.ssh --mount type=bind,src=${HOME}/ansible,dst=/etc/ansible  -d -t cyberxsecurity/ansible sleep infinity
 
